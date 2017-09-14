d6c02111d8571c33acd33592ff6776bfe3028ebf
Caspar Sepp <casparsepp@users.noreply.github.com>
2017-09-14 10:47:03 +0300

    lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Caspar Sepp</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	
+	
+	
+	?>
+
+</body>
+</html>
