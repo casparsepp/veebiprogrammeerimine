3cf40603dc3d680262ff477023f3f2d05b84328d
Caspar Sepp <casparsepp@users.noreply.github.com>
2017-09-21 11:50:41 +0300

    tund 3 directory

---
+++ tund3/avaleht.php
@@ -0,0 +1,23 @@
+<?php
+	//muutujad
+	$myName = "Caspar";
+	$myFamilyName ="Sepp";
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Veebiprogrammeerimine</title>
+</head>
+
+<body>
+	<h1><?php echo $myName ." " .$myFamilyName; ?> </h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Meie õpime <a href="http://www.tlu.ee/">Tallinna ülikoolis</a>.</p>
+	<p>Minu esimene php leht asub <a href="../esimene.php">siin</a>.</p>
+	<p>Minu sõbra Silveri leht on <a href="../../../~kaugsilv/veebiprogrammeerimine">siin</a>.</p>
+	<p>Pilte näeb <a href="foto.php">siin</a>.</p>
+</body>
+</html>
